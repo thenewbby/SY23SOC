06425867d8c49388becc1aac6840848de3ba9c50
Kyrie <duv.cle@gmail.com>
2017-11-03 20:31:16 +0100

    ajout d'un dossier prediviseur

--- Timer/prediviseur_test.vhdl
+++
@@ -1,1 +0,0 @@
---test bench file for prediviseur component
---
+++ prediviseur/prediviseur_test.vhdl
@@ -0,0 +1,6 @@
+--test bench file for prediviseur component
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use std.textio.all;
+use ieee.std_logic_textio.all;
+use WORK.all;
