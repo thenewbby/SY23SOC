ecbabfd4680957aa4755fdeda5af1d41c6c92165
Alexis CARE <alx.care@gmail.com>
2017-11-06 15:29:18 +0100

    wip: timer

--- Timer/PWM.vhdl
+++ Timer/PWM.vhdl
@@ -2,61 +2,57 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
-entity PWM is -- Tu utilisais PWM et pas PWM_1
-  port (  cpt_max, OCR1x_in : in std_logic_vector(7 downto 0);
+entity PWM is
+  port (  OCR1x_in, TCNT1_in : in std_logic_vector(7 downto 0);
           mode_sortie : in std_logic_vector(1 downto 0);
           force, active, PFC_mode, out_inverse, rst, clk : in std_logic;
           OC1x, OC1xbar : out std_logic);
 end entity;
 
 architecture arch_PWM of PWM is
-  signal cpt : std_logic_vector(7 downto 0);
+  -- signal cpt : std_logic_vector(7 downto 0);
   signal OC1x_interne : std_logic;
 
 begin
-  --if force = '1' generate
-  --  OC1x <= '1';
-  --end if;
-  -- OC1x <= '0'; -- Ici, tu disais de mettre OC1x toujours à 0
-  OC1x <= OC1x_interne; -- J'ai cablé OC1x_interne à la sortie
-  OC1xbar <= not OC1x_interne; -- et son inverse
+
+  OC1x <= OC1x_interne;
+  OC1xbar <= not OC1x_interne;
 
   pwm_proc : process(clk, rst, active)
     variable PFC_montant : natural;
   begin
-    --OC1x <= '0';
 
     if rst = '1' then
-      cpt <= (others => '0');
+      -- TCNT1_in <= (others => '0');
       PFC_montant := 1;
     elsif rising_edge(clk) then
       if (PFC_mode = '0') then --Fast PWM Mode
-        if cpt < OCR1x_in then
+        if TCNT1_in < OCR1x_in then
           OC1x_interne <= '0';
-          cpt <= std_logic_vector(unsigned(cpt) + 1);
-        elsif (cpt < cpt_max) then
+          -- TCNT1_in <= std_logic_vector(unsigned(cpt) + 1);
+        elsif (TCNT1_in < cpt_max) then
           OC1x_interne <= '1';
-          cpt <= std_logic_vector(unsigned(cpt) + 1);
+          -- TCNT1_in <= std_logic_vector(unsigned(cpt) + 1);
         else
           OC1x_interne <= '0';
-          cpt <= (others => '0');
+          -- TCNT1_in <= (others => '0');
         end if;
       end if;
     elsif (PFC_mode = '1') then --Phase and frequency correct PWM
-      if (cpt < OCR1x_in)  then
+      if (TCNT1_in < OCR1x_in)  then
         OC1x_interne <= '0';
-        if PFC_montant = 1 or  cpt = "00000000" then
-          cpt <= std_logic_vector(unsigned(cpt) + 1);
+        if PFC_montant = 1 or  TCNT1_in = "00000000" then
+          -- TCNT1_in <= std_logic_vector(unsigned(cpt) + 1);
           PFC_montant := 1;
-        elsif PFC_montant = 0 then
-          cpt <= std_logic_vector(unsigned(cpt) - 1);
-        end if;
-      elsif (cpt <= cpt_max) then
+        -- elsif PFC_montant = 0 then
+          -- TCNT1_in <= std_logic_vector(unsigned(cpt) - 1);
+        -- end if;
+      elsif (TCNT1_in <= cpt_max) then
         OC1x_interne <= '1';
-        if PFC_montant = 1 then
-          cpt <= std_logic_vector(unsigned(cpt) + 1);
-        elsif PFC_montant = 0 or cpt = cpt_max then
-          cpt <= std_logic_vector(unsigned(cpt) - 1);
+        -- if PFC_montant = 1 then
+          -- TCNT1_in <= std_logic_vector(unsigned(cpt) + 1);
+        if PFC_montant = 0 or TCNT1_in = cpt_max then
+          -- TCNT1_in <= std_logic_vector(unsigned(cpt) - 1);
           PFC_montant := 0;
         end if;
       end if;
--- Timer/Timer.vhdl
+++ Timer/Timer.vhdl
@@ -34,15 +34,15 @@ signal reg_TCCR1B : STD_LOGIC_VECTOR (7 downto 0);
 begin
 
 pwm_1 : pwm port map(
-  cpt_max => ,
-  OCR1x_in => ,
-  mode_sortie => ,
+  -- cpt_max => ,
+  OCR1x_in => reg_OCR1A,
+  mode_sortie => reg_TCCR1A(7 downto 6),
   force => ,
   active => ,
   PFC_mode => ,
   out_inverse => ,
-  rst => ,
-  clk => ,
+  rst => Rst,
+  clk => clk,
   OC1x => OC1A,
   OC1xbar => OC1Abar
 );
@@ -50,10 +50,11 @@ pwm_1 : pwm port map(
   proc : process(clk, rst)
     variable adr_int : natural;
     variable rdwr : std_logic_vector(1 downto 0);
+		variable PFC_montant : natural;
   begin
 
     if Rst = '1' then
-
+			reg_TCNT1 <= (others => '0');
 
     elsif rising_edge(clk) then
       adr_int := CONV_INTEGER(unsigned(addr));
@@ -66,12 +67,12 @@ pwm_1 : pwm port map(
           reg_OCR1A <= iowrite;
         end if;
 
-      elsif adr_int = TCNT1 then
+      elsif adr_int = reg_TCNT1 then
 
         if rdwr = "10" then
-          ioread <= reg_TCNT1;
+          ioread <= reg_reg_TCNT1;
         elsif rdwr = "01" then
-          reg_TCNT1 <= iowrite;
+          reg_reg_TCNT1 <= iowrite;
         end if;
 
       elsif adr_int = TCCR1B then
@@ -89,6 +90,24 @@ pwm_1 : pwm port map(
         elsif rdwr = "01" then
           reg_TCCR1A <= iowrite;
         end if;
+
+				if (PFC_mode = '0') then --Fast PWM Mode
+	          reg_TCNT1 <= std_logic_vector(unsigned(cpt) + 1);
+	      end if;
+	    elsif (PFC_mode = '1') then --Phase and frequency correct PWM
+	      if (reg_TCNT1 < OCR1x_in)  then
+	        if PFC_montant = 1 or  reg_TCNT1 = "00000000" then
+	          reg_TCNT1 <= std_logic_vector(unsigned(cpt) + 1);
+	        elsif PFC_montant = 0 then
+	          reg_TCNT1 <= std_logic_vector(unsigned(cpt) - 1);
+	        end if;
+	      else
+	        if PFC_montant = 1 then
+	          reg_TCNT1 <= std_logic_vector(unsigned(cpt) + 1);
+	        elsif PFC_montant = 0 or reg_TCNT1 = cpt_max then
+	          reg_TCNT1 <= std_logic_vector(unsigned(cpt) - 1);
+	        end if;
+	      end if;
 
       end if;
 
