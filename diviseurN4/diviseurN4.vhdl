07c94e1b14e4da5aad28736462c97d411a38f2a5
Alexis CARE <alx.care@gmail.com>
2017-11-09 20:56:00 +0100

    debug behavior

--- diviseurN4/diviseurN4.vhdl
+++ diviseurN4/diviseurN4.vhdl
@@ -21,26 +21,40 @@ signal cpt, cpt_next, clk_div : integer;
 signal clk_interne : std_logic;
 signal null_vect : std_logic_vector(N-1 downto 0);
 signal undet_vect : std_logic_vector(N-1 downto 0);
+
+begin
+	null_vect <= (others => '0');
+	undet_vect <= (others => 'U');
+
+counter : process(rst, clk, cpt, clk_div, pow_div)
 begin
 
-counter : process(cpt, clk_div,pow_div)
-begin
-  null_vect <= (others => '0');
-  undet_vect <= (others => 'U');
-  if (pow_div = null_vect) or (pow_div = undet_vect)then
-    clk_interne <= '0';
-  else
-    clk_div <= 2**(to_integer(unsigned(pow_div)) -1);
+	if rst = '1' then
 
-     if cpt = clk_div then
-         clk_interne <= '1';
-         cpt_next <= 0;
-     else
-         cpt_next <= cpt +1;
-         clk_interne <= '0';
-     end if;
+			cpt_next <= 0;
+			state:=0;
+
+		else if rising_edge(clk) then
+
+			cpt <= cpt_next;
+
+		  if (pow_div = null_vect) or (pow_div = undet_vect)then
+		    clk_interne <= '0';
+  		else
+    		clk_div <= 2**(to_integer(unsigned(pow_div)) -1);
+
+     		if cpt = clk_div then
+         	clk_interne <= '1';
+         	cpt_next <= 0;
+     		else
+         	cpt_next <= cpt +1;
+         	clk_interne <= '0';
+     		end if;
+	 		end if;
+		end if;
   end if;
 end process counter;
+
 clock_out : process(clk_interne)
 begin
   if rising_edge(clk_interne) and state = 1 then
@@ -51,21 +65,6 @@ begin
 		state := 1;
 	end if;
 end process;
-synchro : process(clk, rst)
-begin
 
-	if rst = '1' then
-
-		cpt <= 0;
-		state:=0;
-
-	else if rising_edge(clk) then
-
-		cpt <= cpt_next;
-
-		end if;
-	end if;
-
-end process synchro;
 
 end Behavioral;
