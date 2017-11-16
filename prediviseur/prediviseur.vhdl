24d50c75698fbec51145df29be66ebbe95d79a31
Alexis CARE <alx.care@gmail.com>
2017-11-16 20:03:47 +0100

    commentaire prediv

--- prediviseur/prediviseur.vhdl
+++ prediviseur/prediviseur.vhdl
@@ -7,18 +7,20 @@ use work.all;
 
 entity prediviseur is
 Generic(N : positive := 2);
-Port (	clk : in std_logic;
+Port (
+		--Inputs
+		clk : in std_logic;
 		rst, rst_addr : in std_logic;
 		pow_div : in std_logic_vector(N-1 downto 0);
+		--Outputs
 		clk_out : out std_logic
 	 );
 end prediviseur;
 
 architecture Behavioral of prediviseur is
 
---shared variable state : integer;
+-- definition des signaux
 signal cpt, cpt_next, clk_div : integer;
--- signal clk_interne : std_logic;
 signal null_vect : std_logic_vector(N-1 downto 0);
 signal undet_vect : std_logic_vector(N-1 downto 0);
 begin
@@ -26,37 +28,33 @@ begin
 counter : process(clk, rst, cpt, clk_div,pow_div)
 begin
 
+	--reset
 	if rst = '1' then
-
 		cpt_next <= 0;
 		cpt <= 0;
-		-- state:=0;
-
 	else
+
+		--sur le front montant
 		if rising_edge(clk) then
-
-		cpt <= cpt_next;
-
+			--changement d'etat
+			cpt <= cpt_next;
 		end if;
 
-		-- if rising_edge(clk_interne) and state = 1 then
-		-- 		clk_out <= '0';
-		-- 		state := 0;
-		-- elsif rising_edge(clk_interne) and state = 0 then
-		-- 	clk_out <= '1';
-		-- 	state := 1;
-		-- end if;
+		--division de la clock
+		if (pow_div = null_vect) then
+			clk_interne <= '0';
+		else
+			clk_div <= 2**(to_integer(unsigned(pow_div)-1));
 
-    clk_div <= 2**(to_integer(unsigned(pow_div)-1));
-
-     if cpt = clk_div then
-         clk_out <= '1';
-         cpt_next <= 0;
-     else
-         cpt_next <= cpt +1;
-         clk_out <= '0';
-     end if;
-	 end if;
+			if cpt = clk_div then
+				 clk_interne <= '1';
+				 cpt_next <= 0;
+			else
+				 cpt_next <= cpt +1;
+				 clk_interne <= '0';
+			end if;
+		end if;
+	end if;
 end process counter;
 
 end Behavioral;
